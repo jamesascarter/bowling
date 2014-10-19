54d2fb7888c02c0fc987936212dde999b5f3584b
jamesascarter <jamesascarter@gmail.com>
2014-10-19 17:58:58 +0100

    created score array for single frame. Can add bowl to score

--- spec/pin_spec.js
+++ spec/pin_spec.js
@@ -1,12 +1,16 @@
 describe("Pin", function(){
 
-	it("should be upright once is created", function(){
+	beforeEach(function(){
+
 		pin = new Pin
+		frame = new Frame
+	});
+
+	it("should be upright once is created", function(){
 		expect(pin.isUpright).toBe(true);
 	});
 
 	it("should change to down when hit", function(){
-		pin = new Pin
 		pin.hit(pin)
 		expect(pin.isUpright).toBe(false)
 	});
@@ -15,15 +19,24 @@ describe("Pin", function(){
 describe("Frame", function(){
 
 	it("should contain pins", function(){
-		frame = new Frame
 		expect(frame.pins instanceof Array).toEqual(true)
 	});
 
 	it("should contain 10 pins", function(){
-		frame = new Frame
-		pin = new Pin
-		frame.addPins(pin)
 		expect(frame.pins.length).toEqual(10)
+	});
+
+	it("should be able to knock down a number of pins", function(){
+		frame.bowl(0, 5)
+		expect(frame.pins.length).toEqual(5)
+	});
+
+	it("should add result to the score", function(){
+		frame1 = new Frame
+		frame1.bowl(0,4)
+		expect(frame1.score).toEqual([4])
+
+
 	});
 
 });
--- src/pin.js
+++ src/pin.js
@@ -3,17 +3,22 @@ function Pin(){
 	this.isUpright = true
 };
 
-function Frame() {
-
-	this.pins = []
-
-};
-
 Pin.prototype.hit = function(pin) {
 	this.isUpright = false
 };
 
-Frame.prototype.addPins = function(pin) {
-		for (i=0; i < 10; i++){
+function Frame() {
+
+	this.score = [];
+
+	this.pins = [];
+
+	for (i=0; i < 10; i++){
 			this.pins.push(pin)};
+
+};
+
+Frame.prototype.bowl = function(index, number) {
+	this.pins.splice(index, number)
+	this.score.push(number)
 };
