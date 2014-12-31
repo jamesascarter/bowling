3242a946b78810b3f89202e519171742e04374be
jamesascarter <jamesascarter@gmail.com>
2014-12-31 15:54:35 +0000

    10th frame now contains 10 pins

---
+++ spec/tenthFrame_spec.js
@@ -0,0 +1,17 @@
+describe("tenthFrame", function(){
+  var pin;
+  var tenthFrame;
+
+  beforeEach(function(){
+
+    tenthFrame = new TenthFrame(pin);
+    pin = new Pin;
+  });
+
+  it('should contain 10 pins', function(){
+    expect(tenthFrame.pins.length).toEqual(10)
+  });
+
+  it('should add ')
+
+  });
---
+++ src/tenthFrame.js
@@ -0,0 +1,9 @@
+function TenthFrame(pin) {
+
+	this.tenthScore = [];
+	this.pins = [];
+
+	for (i=0; i < 10; i++){
+      this.pins.push(pin)
+  };
+};
